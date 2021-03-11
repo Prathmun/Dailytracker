8ce2b43eb5123da43a18c9d1e34abfd3a1722b2e
Prathmun <caleb@genly.us>
2021-03-11 09:09:23 -0800

    More prep for consumptive class

--- consumptiontracker.py
+++ consumptiontracker.py
@@ -8,13 +8,27 @@ date_obj = datetime.date(now)
 date_str = str(date_obj)
 
 class Consumptive:
-    def __init__(self, name, squawker_text, question, selection_tree):
+    def __init__(self, name, verb, input_string):
         sef.name = name
-        self.squawker_text = squawker_text
-        self.question = question
-        self.selection_tree = selection_tree
-        #self.sqauwker caller  
-
+        self.verb = verb
+        self.input_string = input_string
+ 
+    def solicit_input():
+        response =input(self.input_string)
+        general_pickler(self.name, response)
+        return response
+    
+    def squawker():
+        daily_log = (daily_log_checker(name))
+        if len(daily_log) == 0:
+            print("You have not {} {} yet today".format(self.verb, self.name))
+        if len(daily_log) == 1:
+            print("you last {} {} it was {} and you ended it at {}".format(self.verb, self.name, daily_log[-1][1],daily_log[-1][0]))
+        if len(daily_log) >= 2:
+            print("you last {} {} it was {} and you ended it at {}".format(self.verb, self.name, daily_log[-1][1],daily_log[-1][0]))
+            print("and before that you {} {} it was {} and it was at {}".format(self.verb, self.name, daily_log[-2][1],daily_log[-2][0]))
+     
+        
 def daily_log_checker(type):
 
     try:
@@ -39,26 +53,10 @@ def general_pickler(type, input,):
     time_str = str(time_obj)
     
     
-    if type == "food":            
-        obj_to_append = [time_str, input]
-        print(obj_to_append)
-        pickle_dumper(obj_to_append, "food")
-    if type == "weed":
-        obj_to_append = [time_str, input]
-        pickle_dumper(obj_to_append, "weed")
-        print(obj_to_append)
-    if type == "wackage":
-        obj_to_append = [time_str, input]
-        pickle_dumper(obj_to_append, "wackage")
-        print(obj_to_append)
-    if type == "break":
-        obj_to_append = [time_str, input]
-        pickle_dumper(obj_to_append, "break")
-        print(obj_to_append)
-    if type == "cyberspace":
-        obj_to_append = [time_str, input]
-        pickle_dumper(obj_to_append, "cyberspace")
-        print(obj_to_append)
+    obj_to_append = [time_str, input]
+    print(obj_to_append)
+    pickle_dumper(obj_to_append, type)
+
 
 
 def cyberspace_squawker():
@@ -70,8 +68,6 @@ def cyberspace_squawker():
     if len(daily_log) >= 2:
         print("Before your last entrance to cyberspace you were doing: {} and you entered at {}".format(daily_log[-1][1],daily_log[-1][0]))
         print("and before that you did {} at {}".format(daily_log[-2][1],daily_log[-2][0]))
-
-#SQUAWKER COULD BECOME A CLASS
 def break_squawker():
     daily_log = daily_log_checker("break")
     if len(daily_log) == 0:
@@ -112,7 +108,11 @@ def wackage_squawker():
 def interface():
     for i in range(0,30):
         print(" ")
-    
+    #for each in consumptive_list:
+        #print(NUMBER NAME tracker)
+        #daily log tracker(each)
+        #each.squawker()
+        #print("*****************")
     print("1. Food tracker")
     daily_log_checker("food")
     food_squawker()
@@ -133,29 +133,38 @@ def interface():
     daily_log_checker("cyberspace")
     cyberspace_squawker()
     print("*****************")
+    
+    
     for i in range(0,5):
         print(" ")
-    selection = input("Select your tracker")
+        
+        
+    selection = input("Select your tracker")     
     selection = int(selection)
+    
+    #consumptive_list[selection].solicit_input()
     if selection == 1:
         foodstring = input("What did you eat?")
         general_pickler("food", foodstring)
+        
     if selection == 2:
         oil_or_bowl = input("1 for bowl, 2 for oil")
         oil_or_bowl = int(oil_or_bowl)
+        ### OPEN QUESTION ###
         if oil_or_bowl == 1:
             oil_or_bowl = "Bowl"
         if oil_or_bowl == 2:
             oil_or_bowl = "Dose(s) of Oil"
         general_pickler("weed", oil_or_bowl)	
+        
     if selection == 3:
         wackage_quality = input("On a scale of 1-5 rate your experience")
         general_pickler("wackage", wackage_quality)
-    #MEATSPACE QUESTIONS
+        
     if selection == 4:
         break_type =input(" Prior to your reemergence into meatspace, we have just one question. What was that dive about?")
         general_pickler("break", break_type)
-    #CYBERSPACE QUESTIONS
+        
     if selection == 5:
         entrance_activity = input("What were you doing in meatspace before this dive? ")
         general_pickler("cyberspace", entrance_activity)
