6415fc74ff69d57720376178ce03375cc7d70de8
Prathmun <caleb@genly.us>
2021-03-13 05:41:02 -0800

    Added a directory error catcher
    Previously even after the recent refactor, when you added a consumptive you had to make the directory for the logs manualy. Now there's an error catcher that will make a directory in the cwd if there isn't a previously existing folder. Feels good man.

--- consumptiontracker.py
+++ consumptiontracker.py
@@ -1,15 +1,22 @@
 from datetime import *
 import pickle
+import os
 
 def daily_log_checker(type):
 
     try:
         todays_log = pickle.load(open(type + "_logs/" +date_str +".py", "rb"))
+    except (FileNotFoundError):
+            os.mkdir(type + "_logs")
+    try:
+        todays_log = pickle.load(open(type + "_logs/" +date_str +".py", "rb"))
     except (IndexError, IOError, EOFError,):
-        spawner = open((type + "_logs/" +date_str +".py"), "a")
-        spawner.close
-        todays_log = []
-        pickle.dump(todays_log,(open(type + "_logs/" +date_str +".py", "wb")))    
+    
+            spawner = open((type + "_logs/" +date_str +".py"), "a")
+            spawner.close
+            todays_log = []
+            pickle.dump(todays_log,(open(type + "_logs/" +date_str +".py", "wb")))    
+     
     return todays_log
          
 def general_pickler(type, input,):
@@ -29,7 +36,7 @@ def general_pickler(type, input,):
     print(obj_to_append)
     pickle_dumper(obj_to_append, type)
 
-#if an input is empty, it shouldn't get entered into the log
+#potential_edit: if an input is empty, it shouldn't get entered into the log
 now = datetime.now()
 date_obj = datetime.date(now)
 date_str = str(date_obj)
@@ -63,17 +70,15 @@ class Consumptive:
 
 
 food = Consumptive("food", "eaten", "What did you eat?")     
-weed = Consumptive("weed", "consume", "1 for bowl, 2 for oil.")
+weed = Consumptive("weed", "consumed", "1 for bowl, 2 for oil.")
 wackage = Consumptive("wackage", "beaten", "On a scale of 1-5 rate your experience.")
 MEAT = Consumptive("break", "taken", "What were you doing in cyberspace before this break?")
-cyber = Consumptive("cyber", "dive", "What were you doing in meatspace prior to this dive?")
-       
-consumptive_list = [food, weed, wackage, MEAT, cyber]
+cyber = Consumptive("cyber", "dove", "What were you doing in meatspace prior to this dive?")
+caffeine = Consumptive("caffeine", "consumed", "What kind of caffiene did you consume?")       
+water = Consumptive("water", "drank", "How do you feel now that you have quafed a container of water?")
+consumptive_list = [food, weed, wackage, MEAT, cyber, caffeine,water]
 
 #consumptives in consideration for adding
-#caffiene = class Cononsumptive("name', verb", "input_string")
-#water = class Cononsumptive("name', verb", "input_string")
-
 
 
     
